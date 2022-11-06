88e51cf455174b407a3a054dc0c67e7eb18d76dc
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-06 13:38:19 -0400

    feat: Change ServeHTTP to embeted http.Handler version

--- server_test.go
+++ server_test.go
@@ -30,7 +30,7 @@ func TestGETPlayers(t *testing.T) {
 		nil,
 	}
 
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("returns Pepper's score", func(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
@@ -69,7 +69,7 @@ func TestStoreWins(t *testing.T) {
 		nil,
 	}
 
-	server := PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("it returns wins when POST", func(t *testing.T) {
 		player := "Pepper"
@@ -93,7 +93,7 @@ func TestStoreWins(t *testing.T) {
 
 func TestLeague(t *testing.T) {
 	store := StubPlayerStore{}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("it returns 200 on /league", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
