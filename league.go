8e0d4b992039bd547744c0b59e2a64682a14bab6
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-13 22:34:42 -0400

    feat(file_system_store): Add RecordWin functionality

--- league.go
+++ league.go
@@ -6,8 +6,20 @@ import (
 	"io"
 )
 
+type League []Player
+
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+
+	return nil
+}
+
 func NewLeague(rdr io.Reader) ([]Player, error) {
-	var league []Player
+	var league League
 
 	err := json.NewDecoder(rdr).Decode(&league)
 	if err != nil {
