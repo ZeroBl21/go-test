36915d1f7e05df24f3a644cfaa5a377dff57e36c
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-16 21:30:37 -0400

    feat: Implement BlindAlerter

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -11,13 +11,13 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-  store, close, err := poker.FileSystemPlayerStoreFile(dbFileName)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer close()
+	store, close, err := poker.FileSystemPlayerStoreFile(dbFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
 
-  fmt.Println("Let's play poker")
-  fmt.Println("Type {Name} wins to record a win")
-  poker.NewCLI(store, os.Stdin).PlayPoker()
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+	poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter)).PlayPoker()
 }
