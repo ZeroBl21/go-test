c44c6a7070bb8e2ec6a3c1d06be18f13e617b4d4
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-16 21:00:48 -0400

    feat(CLI.go): Add BlindAlerter to PlayPoker() and NewCli()

--- CLI.go
+++ CLI.go
@@ -8,30 +8,33 @@ import (
 )
 
 type BlindAlerter interface {
-  ScheduleAlertAt(duration time.Duration, amount int)
+	ScheduleAlertAt(duration time.Duration, amount int)
 }
 
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	alerter     BlindAlerter
 }
 
 func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
-  return &CLI{
-    playerStore: store,
-    in: bufio.NewScanner(in),
-  }
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+		alerter:     alerter,
+	}
 }
 
 func (cli *CLI) PlayPoker() {
-  userInput := cli.readLine()
+  cli.alerter.ScheduleAlertAt(5*time.Second, 100)
+	userInput := cli.readLine()
 
 	cli.playerStore.RecordWin(extractWinner(userInput))
 }
 
 func (cli *CLI) readLine() string {
-  cli.in.Scan()
-  return cli.in.Text()
+	cli.in.Scan()
+	return cli.in.Text()
 }
 
 func extractWinner(userInput string) string {
